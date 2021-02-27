f755d701c0fef5041f0a00eee06097e7e0dc6389
kwistprod <kwist879@gmail.com>
2021-02-27 10:33:37 +0300

    initial commit

---
+++ src/main/java/Hibertest.java
@@ -0,0 +1,7 @@
+import JPAUtil.JPAUtil;
+
+public class Hibertest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/JPAUtil/JPAUtil.java
@@ -0,0 +1,18 @@
+package JPAUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+    private static EntityManagerFactory emf;
+    static {
+        emf = Persistence.createEntityManagerFactory("TEST");
+    }
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+    public static void close(){
+        emf.close();
+    }
+}
